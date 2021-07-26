be2447d54e7626da4ca346cafd7bdff3d215218a
Milan Medlik <milan.medlik@gmail.com>
2021-07-26 08:12:53 +0200

    Initialize project using Create React App

---
+++ src/pages/public/Home/Home.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>GraphQL Monster JavaScript Template</h1>
+        <p>
+          This page is public and accesible for all visitors / users
+          Edit <code>src/pages/public/Home/Home.jsx</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://graphql.monster/documentation/schema-description"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Schema
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://graphql.monster/documentation/obtain-token"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Obtain Token
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://graphql.org/learn/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GraphQL
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+        </span>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
